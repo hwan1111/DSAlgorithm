502ff0cf9ea7175eded939804c6c6b6867acfde5
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2024-03-31 11:51:53 +0900

    Update BubbleSort.py
    수정(향상)된 버블 정렬 추가

--- Algorithm/Sort/BubbleSort.py
+++ Algorithm/Sort/BubbleSort.py
@@ -5,3 +5,16 @@ def bubbleSort(arr):
                 arr[j], arr[j-1] = arr[j-1], arr[j]
 
     return arr
+# 수정(향상)된 버블 정렬 알고리즘
+def betterbubbleSort(arr):
+    for i in range(lne(arr)):
+        state = True
+        for j in range(len(arr)-1, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                state = False
+
+        if state:
+            break
+
+    return arr
