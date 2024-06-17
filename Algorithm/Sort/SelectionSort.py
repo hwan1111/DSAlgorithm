d6cdacf8e96b010ce91658c288e504b5308ba1c6
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2024-06-17 14:58:18 +0900

    Update SelectionSort.py

--- Algorithm/Sort/SelectionSort.py
+++ Algorithm/Sort/SelectionSort.py
@@ -1,8 +1,8 @@
 def SelectionSort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr)-1):
         k = i
-        for j in range(i, len(arr)):
-            if arr[k] > arr[j]:
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[k]:
                 k = j
 
         arr[k], arr[i] = arr[i], arr[k]
