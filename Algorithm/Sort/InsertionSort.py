46c847076bc91dc8909fa3da3e348512cd28865b
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2024-06-18 21:58:37 +0900

    Update InsertionSort.py
    Add comment.

--- Algorithm/Sort/InsertionSort.py
+++ Algorithm/Sort/InsertionSort.py
@@ -1,10 +1,11 @@
-def insertionSort(arr):
+def insertion_sort(arr):
     for i in range(1, len(arr)):
-        temp = arr[i]
+        temp = arr[i]   # 정렬되지 않은 배열의 첫 번째 요소를 저장
         j = i-1
+        # 정렬된 배열의 요소와 비교하여 삽입할 위치를 찾음
         while j >= 0 and temp < arr[j]:
-            arr[j+1] = arr[j]
-            j -= 1
-        arr[j+1] = temp
+            arr[j+1] = arr[j]   # 현재 요소를 오른쪽으로 이동
+            j -= 1              # 삽입할 위치를 찾기 위해 인덱스를 감소시킴
+        arr[j+1] = temp     # 저장된 요소를 올바른 위치에 삽입
 
     return arr
